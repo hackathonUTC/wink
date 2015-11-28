0076bab63dd592966b9d178759483b04daec1b4d
Nicolas <nicolas.terisse@gmail.com>
2015-11-28 22:35:13 +0100

    uncomplete creation of getMatchingLevel and poincarre probability function

--- engine.js
+++ engine.js
@@ -1,5 +1,72 @@
-var json = require("./representation");
-var profile = json[0];
-var criterias = json[1];
+var content = require("./representation");
 
-console.log(profile);
+(function poincarre(tab)
+	var len = tab.length;
+
+	var somme1 = 0;
+	var somme2 = 0;
+	var prod;
+	var i;
+	var j;
+	var coeff;
+	for(k=1; k<=len; i++){
+		prod = 1;
+		coeff = Math.pow(-1, i-1);
+		for(i=1; i<=k; j++){
+			for(j=1; j<=k; j++){
+				prod *= tab[j-1];
+			}
+			somme2 += prod;		
+		}
+		somme1 += somme2;
+	}
+	return somme1;
+)
+(function getMatchingLevel(content) {
+	var profile = content[0];
+	var criterias = content[1];
+
+	var finalValues = []
+
+	for (var criteriaKey in criterias){
+		for(var profileKey in profile){
+			switch(criteriaKey){
+				case "gender":
+					if(criterias[criteriaKey]!=profile[profileKey]){
+						return -1;
+					}
+				case "semester":
+					if(criterias[criteriaKey]==profile[profileKey]){
+						finalValues.push(0.9)
+					}
+					else{
+						finalValues.push(0.1)
+					}
+					break;
+				case "branch":
+					if(criterias[criteriaKey]==profile[profileKey]){
+						finalValues.push(0.8)
+					}
+					else{
+						finalValues.push(0.2)
+					}
+					break;
+				case "origin":
+					if(criterias[criteriaKey]!=profile[profileKey]){
+						return -1;
+					}
+				case "searchRelation":
+					if(criterias[criteriaKey]!=profile[profileKey]){
+						return -1;
+					}
+				case "sport"
+
+
+				}break;
+			}
+		}
+	}
+)
+
+//console.log(eval(profile));
+
