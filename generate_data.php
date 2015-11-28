ccf1ee17f70422fc09028ffa04c5bacc0b3438bd
Titouan Galopin <galopintitouan@gmail.com>
2015-11-28 19:36:34 +0100

    Send EDT

--- generate_data.php
+++ generate_data.php
@@ -1,2 +1,11 @@
 <?php
 
+require 'vendor/autoload.php';
+
+$faker = Faker\Factory::create('fr_FR');
+
+$files = new DirectoryIterator('EDT');
+
+foreach ($files as $file) {
+    echo $file->getBasename('.edt') . "\n";
+}
