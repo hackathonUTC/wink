f11ba1ba868bac36d1a9d77f9df9a5f3e83bb28e
Titouan Galopin <galopintitouan@gmail.com>
2015-11-29 03:48:40 +0100

    Wink

--- app/index.android.js
+++ app/index.android.js
@@ -3,47 +3,41 @@
 var React = require('react-native');
 
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    Image,
+    StyleSheet,
+    ToolbarAndroid,
+    View,
 } = React;
 
 var app = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Wink!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render: function() {
+        return (
+            <View style={styles.container}>
+                <ToolbarAndroid title="Wink" />
+                <Image source={require('./img/logo.png')} />
+            </View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#b90727',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('app', () => app);
