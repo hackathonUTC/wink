47e9eb2df31065f73c4971003d6cc7362b39a409
Thibaut Roy <git@thibaut-roy.fr>
2015-11-29 07:40:14 +0100

    last sql

--- server/tables.sql
+++ server/tables.sql
@@ -1,6 +1,11 @@
-CREATE TABLE user (
-	user_id				INTEGER		NOT NULL PRIMARY KEY,
-	login				VARCHAR(8)	NOT NULL,
+CREATE TABLE origins (
+	origin_id 	SERIAL 	NOT NULL PRIMARY KEY,
+	name		VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE users (
+	user_id				VARCHAR(8)	NOT NULL PRIMARY KEY,
 	firstname			VARCHAR(50),
 	lastname			VARCHAR(50),
 	branch				VARCHAR(5),
@@ -9,52 +14,58 @@ CREATE TABLE user (
 	relationshipStatus	VARCHAR(20)	CHECK (relationshipStatus IN ('single','married','in a relationship', 'complicated')) ,
 	birthday			DATE,
 	semester			INTEGER,
-	typeOfRelation		VARCHAR(6)	CHECK (typeOfRelation IN ('cdi', 'cdd', 'trial'))
-	orientation		 	VARCHAR(6)		CHECK (orientation IN ('m', 'f', 'a')),
+	typeOfRelation		VARCHAR(6)	CHECK (typeOfRelation IN ('cdi', 'cdd', 'trial')),
+	sexualPref		 	VARCHAR(6)	CHECK (sexualPref IN ('m', 'f', 'a')),
 	picture				VARCHAR(100),
-	origin_id			INTEGER	FOREIGN KEY REFERENCES origin(origin_id)
+	origin_id			INTEGER	REFERENCES origins(origin_id)
 );
 
 
-CREATE TABLE origin (
-	origin_id 	INTEGER 	NOT NULL PRIMARY KEY,
-	name		VARCHAR(50) NOT NULL
-)
+CREATE TABLE tokens (
+	user_id		VARCHAR(8) 	NOT NULL REFERENCES users(user_id),
+	token		VARCHAR(50)	NOT NULL,
+	expire_at	DATE		NOT NULL
+);
 
+CREATE TABLE user_swipe (
+	userFrom_id	VARCHAR(8)	NOT NULL REFERENCES users(user_id),
+	userTo_id	VARCHAR(8)	NOT NULL REFERENCES users(user_id),
+	type	 	CHAR(1) NOT NULL CHECK (type IN ('l','r'))
+);
 
-CREATE TABLE hobby (
-	hobby_id	INTEGER 	NOT NULL PRIMARY KEY,
+CREATE TABLE hobbies (
+	hobby_id	SERIAL 	NOT NULL PRIMARY KEY,
 	name 		VARCHAR(50) NOT NULL
-)
+);
 CREATE TABLE user_hobby (
-	user_id 	INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
-	hobby_id	INTEGER NOT NULL FOREIGN KEY REFERENCES hobby(hobby_id)
-)
+	user_id 	VARCHAR(8) NOT NULL REFERENCES users(user_id),
+	hobby_id	INTEGER NOT NULL REFERENCES hobbies(hobby_id)
+);
 
-CREATE TABLE organization (
-	organization_id	INTEGER 	NOT NULL PRIMARY KEY (organization_id),
+CREATE TABLE organizations (
+	organization_id	SERIAL	 	NOT NULL PRIMARY KEY,
 	name 			VARCHAR(50) NOT NULL
-)
+);
 CREATE TABLE user_organization (
-	user_id			INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
-	organization_id	INTEGER NOT NULL FOREIGN KEY REFERENCES organization(organization_id)
-)
+	user_id			VARCHAR(8) NOT NULL REFERENCES users(user_id),
+	organization_id	INTEGER NOT NULL REFERENCES organizations(organization_id)
+);
 
-CREATE TABLE sport (
-	sport_id	INTEGER 	NOT NULL PRIMARY KEY (sport_id),
+CREATE TABLE sports (
+	sport_id	SERIAL	 	NOT NULL PRIMARY KEY,
 	name 		VARCHAR(50) NOT NULL
-)
+);
 CREATE TABLE user_sport (
-	user_id		INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
-	sport_id 	INTEGER NOT NULL FOREIGN KEY REFERENCES organization(organization_id)
-)
+	user_id		VARCHAR(8) NOT NULL REFERENCES users(user_id),
+	sport_id 	INTEGER NOT NULL REFERENCES organizations(organization_id)
+);
 
 
-CREATE TABLE course (
-	course_id	INTEGER 	NOT NULL PRIMARY KEY,
+CREATE TABLE courses (
+	course_id	SERIAL	 	NOT NULL PRIMARY KEY,
 	name 		VARCHAR(50) NOT NULL
-)
+);
 CREATE TABLE user_course (
-	user_id 	INTEGER NOT NULL FOREIGN KEY REFERENCES user(user_id),
-	course_id	INTEGER NOT NULL FOREIGN KEY REFERENCES course(course_id)
-)
+	user_id 	VARCHAR(8) NOT NULL REFERENCES users(user_id),
+	course_id	INTEGER NOT NULL REFERENCES courses(course_id)
+);
