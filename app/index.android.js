305657169a3bd909246fff131ac338adfbe83caf
Titouan Galopin <galopintitouan@gmail.com>
2015-11-29 05:39:15 +0100

    Work on main view

--- app/index.android.js
+++ app/index.android.js
@@ -15,7 +15,12 @@ var app = React.createClass({
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <Image source={require('./img/logo.png')} style={styles.image} />
+                    <Image source={require('./img/icons/menu.png')} style={styles.headerMenuIcon} />
+                    <Image source={require('./img/logo.png')} style={styles.headerLogo} />
+                    <Image source={require('./img/icons/shots.png')} style={styles.headerShotsIcon} />
+                </View>
+                <View style={styles.main}>
+                    <Text>Hello</Text>
                 </View>
             </View>
         );
@@ -26,18 +31,37 @@ var styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
-        alignItems: 'flex-start',
+        alignItems: 'flex-start'
     },
     header: {
+        flexDirection: 'row',
         position: 'absolute',
         left: 0,
         right: 0,
         height: 45,
         backgroundColor: '#b90727',
+        justifyContent: 'center'
     },
-    image: {
+    headerLogo: {
         marginTop: 5,
         marginLeft: 10
+    },
+    headerMenuIcon: {
+        position: 'absolute',
+        left: 10,
+        top: 10
+    },
+    headerShotsIcon: {
+        position: 'absolute',
+        right: 10,
+        top: 10
+    },
+    main: {
+        flexDirection: 'column',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 45
     }
 });
 
